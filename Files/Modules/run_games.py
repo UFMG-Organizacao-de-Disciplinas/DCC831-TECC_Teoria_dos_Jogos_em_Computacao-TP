f63d72026189ad264b0d61a3780d02b46fba34e8
rangel-avila <rangelavilabarroso@gmail.com>
2025-07-05 16:26:47 -0300

    Update run_games.py
    Update in the run_games, focusing in formatting the functions and trying to implement the run of the game

--- Files/Modules/run_games.py
+++ Files/Modules/run_games.py
@@ -5,21 +5,19 @@ if the number of players is odd, one player is left to play alone.
 It uses the `random` module to shuffle the player list and create pairs.
 """
 
-def draw_unique_players(players, alone_player):
+def draw_unique_players(players: list, alone_player: list | None = None) -> list:
     """ Function receives as input the player list, and returns the list of players
     per game ordering
 
     Args:
         players (list): List of players to be drawn.
-        alone_player (list | None): Optional player who will play alone if
+        alone_player (list | None): Optional player who will stay for next round if
             the number of players is odd.
 
     Returns:
-        tuple: A tuple containing:
-            - player_sequence (list): A list of tuples, each containing two players
-            who will play together.
-            - alone_player (list | None): The player who is left alone if the number
-            of players is odd.
+        player_sequence (list): List with a tuple of the players drawn.
+        alone_player (list | None): Optional player who will stay for next round if
+            the number of players is odd.
     """
 
     import random
@@ -51,6 +49,38 @@ def draw_unique_players(players, alone_player):
         alone_player = None
 
     return player_sequence, alone_player
+
+
+
+
+
+
+def lets_play_the_game(player_sequence,strategies,game_name):
+
+  # Assigning the label of each player to p1 and p2
+  p1 = player_sequence[0]
+  p2 = player_sequence[1]
+
+
+  #Run of the game for player 1 based on his strategy
+  action_1 = get_strategies()[strategies[p1]](game_name, 1)
+
+  #Run of the game for player 2 based on his strategy
+  action_2 = get_strategies()[strategies[p2]](game_name, 2)
+
+  #Payoff for player 1 at (action_1, action_2) profile
+  payoff_1 = get_payoffs(game_name)[1][action_1][action_2]
+
+  #Run of the game for player 2 based on his strategy
+  payoff_2 = get_payoffs(game_name)[2][action_1][action_2]
+
+  return payoff_1, payoff_2
+
+
+
+
+
+
 
 # Testing of the game (not necessarily in the final module)
 players = ['Rangel', 'João', 'Hard-Enzo']
